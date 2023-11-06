8d76f336d3587e6b48a4912861bd000c2d30837b
febryanpratama <spandupan46@student.untan.ac.id>
2023-11-06 16:15:57 +0700

    feat: setup layout

---
+++ resources/views/_partials/header.blade.php
@@ -0,0 +1,140 @@
+<header id="masthead" class="site-header">
+    <div class="container">
+        <div class="hgroup-wrap d-flex justify-content-between align-items-sm-center">
+            <div class="site-identity">
+                <p class="site-title">
+                    <a href="index-2.html">
+                        <img src="{{ asset('') }}assets/img/white-logo.png" alt="logo">
+                    </a>
+                </p>
+            </div>
+            <div class="main-navigation">
+                <nav id="navigation" class="navigation d-none d-lg-inline-block">
+                    <ul>
+                        <li class="menu-item-has-children">
+                            <a href="#">home</a>
+                            <ul>
+                                <li>
+                                    <a href="index-2.html">home Version 1</a>
+                                </li>
+                                <li class="current-menu-item">
+                                    <a href="index-v2.html">home version 2</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="about-us.html">about us</a>
+                        </li>
+                        <li class="menu-item-has-children">
+                            <a href="#">pages</a>
+                            <ul>
+                                <li>
+                                    <a href="service.html">service</a>
+                                </li>
+                                <li>
+                                    <a href="portfolio.html">portfolio</a>
+                                </li>
+                                <li>
+                                    <a href="portfolio-detail.html">portfolio detail</a>
+                                </li>
+                                <li>
+                                    <a href="team.html">team</a>
+                                </li>
+                                <li>
+                                    <a href="team-detail.html">team detail</a>
+                                </li>
+                                <li>
+                                    <a href="Career.html">carreer</a>
+                                </li>
+                                <li>
+                                    <a href="Career-Detail.html">carreer detail</a>
+                                </li>
+                                <li>
+                                    <a href="faq.html">faq</a>
+                                </li>
+                                <li>
+                                    <a href="testimonial.html">testimonial</a>
+                                </li>
+                                <li>
+                                    <a href="search.html">search result</a>
+                                </li>
+                                <li>
+                                    <a href="product-list.html">product list</a>
+                                </li>
+                                <li>
+                                    <a href="single-product.html">single product</a>
+                                </li>
+                                <li>
+                                    <a href="comming-soon.html">coming soon</a>
+                                </li>
+                                <li>
+                                    <a href="404page.html">404 page</a>
+                                </li>
+                                <li>
+                                    <a href="cart.html">Shop Cart</a>
+                                </li>
+                                <li>
+                                    <a href="product-cheakout.html">Cheakout</a>
+                                </li>
+                                <li>
+                                    <a href="single-page.html">single-page</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="menu-item-has-children">
+                            <a href="#">blog</a>
+                            <ul>
+                                <li>
+                                    <a href="single-post.html">blog list</a>
+                                </li>
+                                <li>
+                                    <a href="single-blog.html">single blog</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="contact.html">contact</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="side-nav d-flex justify-content-between align-items-center">
+                <div class="header-icon-group">
+                    <div class="header-social social-links">
+                        <ul>
+                            <li>
+                                <a href="https://www.facebook.com/">
+                                    <i class="fab fa-facebook"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://twitter.com/?lang=en">
+                                    <i class="fab fa-twitter"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.youtube.com/">
+                                    <i class="fab fa-youtube"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://www.instagram.com/">
+                                    <i class="fab fa-instagram"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="header-search-icon d-inline-block">
+                        <a href="#" class="search-icon">
+                            <i class="fas fa-search"></i>
+                        </a>
+                    </div>
+                </div>
+                <div class="header-btn d-inline-block">
+                    <a href="contact.html" class="button-round-primary">Get A Quote</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="mobile-menu-container"></div>
+</header>
