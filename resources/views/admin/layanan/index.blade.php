274917cfdbc70bc706ddc4d7d277dc0b4acfef9f
Febryan Pratama <febryancaesarpratama@gmail.com>
2024-02-29 14:35:49 +0700

    feat: rev layanan

--- resources/views/admin/layanan/index.blade.php
+++ resources/views/admin/layanan/index.blade.php
@@ -14,37 +14,76 @@
        </div>
        <!-- /BREADCRUMB -->
        <div class="row layout-top-spacing">
-          <div class="col-xl-12 col-lg-12 col-sm-12  layout-spacing">
-             <div class="d-flex justify-content-between m-2">
-                 <h4><b>Konten Persyaratan ISBN</b></h4>
-                 {{-- <button type="button" class="btn btn-primary mr-2 _effect--ripple waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#addSSejarah">
-                     + Team
-                 </button> --}}
-             </div>
-             <div class="widget-content widget-content-area br-8 card">
-                 <form action="{{ url('admin/layanan/pengajuan-isbn') }}" method="POST" enctype="multipart/form-data">
-                     @csrf
-                     <div class="col-md-12 mt-3">
-                         <label for="" class="control-label">Konten Pengajuan ISBN</label>
-                         <textarea name="konten"  id="editor" cols="30" rows="10"></textarea>
-                     </div>
-
-                     <div class="col-md-12 mt-3">
-                        <label for="" class="control-label">Harga Pengajuan</label>
-                        <input type="number" class="form-control" name="harga">
-                     </div>
-                     <div class="col-md-12 mt-3">
-                         <button type="submit" class="form-control btn btn-outline-primary">Simpan Konten</button>
-                     </div>
-                 </form>
-                 
-             </div>
-          </div>
+            <div class="card">
+                <div class="card-body">
+                   <div class="col-xl-12 col-lg-12 col-sm-12  layout-spacing">
+                      <div class="d-flex justify-content-between m-2">
+                          <h4><b>Konten Persyaratan ISBN</b></h4>
+                          {{-- <button type="button" class="btn btn-primary mr-2 _effect--ripple waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#addSSejarah">
+                              + Team
+                          </button> --}}
+                          {{-- {{ dd($pengajuanisbn) }} --}}
+                      </div>
+                      <div class="widget-content widget-content-area br-8 card">
+                          <form action="{{ url('admin/layanan/pengajuan-isbn') }}" method="POST" enctype="multipart/form-data">
+                              @csrf
+                              <div class="col-md-12 mt-3">
+                                  <label for="" class="control-label">Konten Pengajuan ISBN</label>
+                                  <textarea name="konten"  id="editor" cols="30" rows="10">{{ $pengajuanisbn->konten }}</textarea>
+                              </div>
+         
+                              <div class="col-md-12 mt-3">
+                                 <label for="" class="control-label">Harga Pengajuan</label>
+                                 <input type="number" class="form-control" name="harga" value="{{ $pengajuanisbn->harga }}">
+                              </div>
+                              <div class="col-md-12 mt-3">
+                                  <button type="submit" class="form-control btn btn-outline-primary">Simpan Konten</button>
+                              </div>
+                          </form>
+                          
+                      </div>
+                   </div>
+                </div>
+            </div>
+       </div>
+       
+       <div class="row layout-top-spacing">
+            <div class="card">
+                <div class="card-body">
+                   <div class="col-xl-12 col-lg-12 col-sm-12  layout-spacing">
+                      <div class="d-flex justify-content-between m-2">
+                          <h4><b>Konten Persyaratan ISBN</b></h4>
+                          {{-- <button type="button" class="btn btn-primary mr-2 _effect--ripple waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#addSSejarah">
+                              + Team
+                          </button> --}}
+                          {{-- {{ dd($pengajuanisbn) }} --}}
+                      </div>
+                      <div class="widget-content widget-content-area br-8 card">
+                          <form action="{{ url('admin/layanan/pengajuan-isbn') }}" method="POST" enctype="multipart/form-data">
+                              @csrf
+                              <div class="col-md-12 mt-3">
+                                  <label for="" class="control-label">Konten Pengajuan ISBN</label>
+                                  <textarea name="konten"  id="editor" cols="30" rows="10">{{ $pengajuanisbn->konten }}</textarea>
+                              </div>
+         
+                              <div class="col-md-12 mt-3">
+                                 <label for="" class="control-label">Harga Pengajuan</label>
+                                 <input type="number" class="form-control" name="harga" value="{{ $pengajuanisbn->harga }}">
+                              </div>
+                              <div class="col-md-12 mt-3">
+                                  <button type="submit" class="form-control btn btn-outline-primary">Simpan Konten</button>
+                              </div>
+                          </form>
+                          
+                      </div>
+                   </div>
+                </div>
+            </div>
        </div>
     </div>
  </div>
  
- <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+ {{-- <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
      <div class="modal-dialog" role="document">
          <div class="modal-content">
              <div class="modal-header">
@@ -79,5 +118,5 @@
              </form>
          </div>
      </div>
- </div>
+ </div> --}}
 @endsection
